a0167d849674d8cac8bb8de82f89231d2aadf1a0
MaazDanish <dk599318@gmail.com>
2023-09-19 10:58:44 +0530

    Filter added from front end we are filtering

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+document.getElementById('search').addEventListener('keyup' , filterItems);
+
+
 async function OnSubmit(event) {
     event.preventDefault();
     const name = event.target.name.value;
@@ -30,29 +34,18 @@ async function OnSubmit(event) {
     event.target.rating.value = '';
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const searchForm = document.querySelector('.search-form');
-    // const searchResults = document.querySelector('#reviewsBox');
-
-    searchForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-
-        const companyName = document.getElementById('companyName').value;
-        const url = `/search?companyName=${encodeURIComponent(companyName)}`;
-
-        try {
-            const response = await fetch(url);
-            if (response.ok) {
-                const data = await response.json();
-                display(data);
-            } else {
-                console.error(`Error: ${response.status} - ${response.statusText}`);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', async () => {
+//     try {
+//         let res = await axios.get('http://localhost:4000/review');
+//         console.log(res.data);
+//         for (var i = 0; i < res.data.length; i++) {
+//             display(res.data[i]);
+//         }
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// });
 
 
 
@@ -62,10 +55,12 @@ async function display(reviewObj) {
 
     const h2 = document.createElement('h2');
     const h3 = document.createElement('h2');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
+    const p1 = document.createElement('p')
     const p3 = document.createElement('p');
-    const p4 = document.createElement('p');
+    const li = document.createElement('li');
+
+    li.className = 'li-js';
+
     const hr = document.createElement('hr');
 
 
@@ -75,10 +70,9 @@ async function display(reviewObj) {
     //  setting the  text content
     h2.textContent = 'Company Name : ' + name;
     h3.textContent = 'Company rating : ' + rating;
-    p1.textContent = 'Pros :';
-    p2.textContent = pros;
-    p3.textContent = 'cons :';
-    p4.textContent = cons;
+    p1.textContent = 'Pros : ' + pros;
+    p3.textContent = 'cons : ' + cons;
+
 
     // let count = 0;
     // let newRating = 0;
@@ -88,25 +82,31 @@ async function display(reviewObj) {
     // }
 
     //  Appending the child element to the parent element
-    ParentElement.appendChild(h2);
-    ParentElement.appendChild(h3);
-    ParentElement.appendChild(p1);
-    ParentElement.appendChild(p2);
-    ParentElement.appendChild(p3);
-    ParentElement.appendChild(p4);
-    ParentElement.appendChild(hr)
+    li.appendChild(h2)
+    li.appendChild(h3)
+    li.appendChild(p1)
+    li.appendChild(p3)
+    li.appendChild(hr)
+    ParentElement.appendChild(li);
+    // ParentElement.appendChild(h2);
+    // ParentElement.appendChild(h3);
+    // ParentElement.appendChild(p1);
+    // ParentElement.appendChild(p3);
+    // ParentElement.appendChild(hr)
 }
 
-// window.addEventListener('DOMContentLoaded', async () => {
-//     try {
-//         let res = await axios.get('http://localhost:4000/review');
-//         for (var i = 0; i < res.data.length; i++) {
-//             console.log(res.data[i]);
-//             display(res.data[i]);
-//         }
-//     } catch (err) {
-//         console.log(err);
-//     }
-// });
+//  filtering items
 
+function filterItems(e){
+    let text = e.target.value;
+    let ul = document.getElementById('reviewsBox')
+    Array.from(ul.getElementsByClassName('li-js')).forEach(item =>{ 
+       if(item.textContent.indexOf(text) !== -1){
+           item.style.display='block'
+        } else{
+            item.style.display='none';
+       }
+    
+    })
+}
 
